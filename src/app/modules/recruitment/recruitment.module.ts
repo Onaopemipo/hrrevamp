f1a412ad3358b22716de5f88aff30923ef738682
Onaopemipo <philipmuyiwa@gmail.com>
2021-02-12 10:14:46 +0100

    commitConflict

--- src/app/modules/recruitment/recruitment.module.ts
+++ src/app/modules/recruitment/recruitment.module.ts
@@ -1,3 +1,7 @@
+import { AccountVerifyRoutingModule } from './account-verify/accountverify-routing.module';
+import { RecruitmentRoutingModule } from './recruitment-routing.module';
+import { ApplicantssignupComponent } from './applicantssignup/applicantssignup.component';
+import { ApplicantssigninComponent } from './applicantssignin/applicantssignin.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeModule } from '../../@theme/theme.module';
@@ -10,28 +14,23 @@ import {
   NbIconModule,
   NbInputModule,
 } from '@nebular/theme';
+import { FormsModule as ngFormsModule } from '@angular/forms';
 import { RecruitmentComponent } from './recruitment.component';
-import { ApplicantssigninComponent } from './applicantssignin/applicantssignin.component';
-import { NewPasswordComponent } from './passwordReset/new-password/new-password.component';
+import { AccountVerifyComponent } from './account-verify/account-verify.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
-import { ApplicantssignupComponent } from './applicantssignup/applicantssignup.component';
-import { RecruitmentRoutingModule } from './recruitment-routing.module';
-// import { FormsModule as ngFormsModule } from '@angular/forms';
-// import { RecruitmentComponent } from './recruitment.component';
-// import { AccountVerifyComponent } from './account-verify/account-verify.component';
-// import { PasswordResetComponent } from './password-reset/password-reset.component';
-// import { NewPasswordComponent } from './passwordReset/new-password/new-password.component';
-// import { ApplicantsDashboardComponent } from './applicants-dashboard/applicants-dashboard.component';
+import { NewPasswordComponent } from './passwordReset/new-password/new-password.component';
+import { ApplicantsDashboardComponent } from './applicants-dashboard/applicants-dashboard.component';
 
 
 
 
 @NgModule({
   declarations: [RecruitmentComponent, ApplicantssigninComponent, ApplicantssignupComponent,
-    PasswordResetComponent, NewPasswordComponent, ApplicantssignupComponent],
+    PasswordResetComponent, NewPasswordComponent, ApplicantsDashboardComponent],
   imports: [
     ThemeModule,
     CommonModule,
+    ngFormsModule,
     NbButtonModule,
     NbCardModule,
     NbCheckboxModule,
