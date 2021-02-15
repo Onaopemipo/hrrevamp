df2dec4df32745c593cddec7230fb30ac5f89c11
Temitope Badmus <tope@smartace.ng>
2021-02-15 09:49:48 +0100

    dashboardUpdate

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
-import  * as Chart from 'chart.js'
+
+
+
+const no_of_ms_in_a_day = 24 * 60 * 60 * 1000;
+
 
 @Component({
   selector: 'ngx-dashboard',
@@ -63,6 +67,14 @@ export class DashboardComponent implements OnInit {
   showLabels = true;
   explodeSlices = false;
   doughView = [700, 280]
+
+
+  //Events
+  weekdays: Date[] = [];
+  today = new Date();
+  days_of_week = [
+    'Mon', 'Tue', 'Wed', 'Thur', 'Fri'
+  ];
  
   constructor(private theme: NbThemeService) { 
     this.colorScheme = {
@@ -132,7 +144,17 @@ export class DashboardComponent implements OnInit {
  
   }
   ngOnInit(): void {
-  
+  //Events
+  const today = this.today;
+  const day_of_week = today.getDay();
+  const start_of_week = Number(today) - day_of_week * no_of_ms_in_a_day;
+  const days = [];
+  for (let day_no = 1; day_no <= 5; day_no++) {
+    const day_in_ms = start_of_week + day_no * no_of_ms_in_a_day;
+    const day = new Date(day_in_ms);
+    days.push(day);
+  }
+  this.weekdays = days;
   }
 
   openModal() {
