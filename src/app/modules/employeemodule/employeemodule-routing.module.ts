fb76ade02ee6a2a8e84d0775185a05f5d8578e89
Abiodun <bazemontana@gmail.com>
2021-02-22 10:10:00 +0100

    updated exitrequest exitmagement leave and profile

--- src/app/modules/employeemodule/employeemodule-routing.module.ts
+++ src/app/modules/employeemodule/employeemodule-routing.module.ts
@@ -2,11 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HiringchecklistComponent } from './hiringchecklist/hiringchecklist.component';
 import { EmployeeonboardingdashboardComponent } from './employeeonboardingdashboard/employeeonboardingdashboard.component';
+import { EmploymentexitmanagementComponent } from './employmentexitmanagement/employmentexitmanagement.component';
+import { ManagementexistComponent } from './managementexist/managementexist.component';
+import { ExistrequestComponent } from './existrequest/existrequest.component';
+
 const routes: Routes = [
   {
     path: '',
     component: EmployeeonboardingdashboardComponent
+},
+{
+  path: 'exitmanagement',
+  component: EmploymentexitmanagementComponent
+},
+{
+  path: 'exitform',
+  component: ManagementexistComponent
+},
+{
+path : 'exitrequest',
+component : ExistrequestComponent
 }
+
 ];
 
 @NgModule({
---
+++ src/app/modules/employeemodule/existrequest/existrequest.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-existrequest',
+  templateUrl: './existrequest.component.html',
+  styleUrls: ['./existrequest.component.scss']
+})
+export class ExistrequestComponent implements OnInit {
+  
+
+  selectedOption: string = ''
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
