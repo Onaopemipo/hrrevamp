07c1ad6659d45916cf5864892bff280b8279e3aa
Onaopemipo <philipmuyiwa@gmail.com>
2021-03-05 11:20:29 +0100

    Update Career Succession

---
+++ src/app/modules/career-succession/talent-pool/talent-pool.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-talent-pool',
+  templateUrl: './talent-pool.component.html',
+  styleUrls: ['./talent-pool.component.scss']
+})
+export class TalentPoolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
