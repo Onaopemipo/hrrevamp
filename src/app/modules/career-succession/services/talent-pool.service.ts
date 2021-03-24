79c0f929c03c526fd53a0dd2f74ec96e4ed628b3
Ridwan <olarid7852@gmail.com>
2021-03-24 10:19:30 +0100

    Implemented talent pool service

---
+++ src/app/modules/career-succession/services/talent-pool.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { CrudService, ListResult } from 'app/_services/base-api.service';
+import { Observable, Subject } from 'rxjs';
+import * as fakerStatic from 'faker';
+
+export class EmployeeName {
+  name: string;
+  id: string;
+
+  fake(id) {
+    this.id = id;
+    this.name = fakerStatic.name.findName() + ' ' + fakerStatic.name.findName();
+    return this;
+  }
+}
+export class MyTalentPool {
+  id: number;
+  title: string;
+  description: string;
+  type: string;
+  requirementPoint: number;
+  skillWeight: number;
+  experience: number;
+  experienceWeight: number;
+  employees: EmployeeName[];
+
+  fake(id) {
+    this.id = id;
+    this.title = fakerStatic.name.title();
+    this.description = fakerStatic.lorem.sentence();
+    this.type = fakerStatic.name.suffix();
+    this.requirementPoint = fakerStatic.random.number();
+    this.skillWeight = fakerStatic.random.number();
+    this.experienceWeight = fakerStatic.random.number();
+    this.employees = [1, 2, 3, 4, 5, 6, 7, 9, 10].map(employee_id => new EmployeeName().fake(employee_id));
+    return this;
+  }
+}
+
+export class TalentPoolFilter {
+
+}
+class MessageOut{
+  isSuccessful: boolean;
+  message: string;
+  redirectUrl: string;
+  retId: 0;
+
+  constructor(message, isSuccesful) {
+    this.message = message;
+    this.isSuccessful = isSuccesful;
+  }
+}
+function createSubscription<T>(data: T) {
+  const subject = new Subject<T>();
+  window.setTimeout(() => {
+    subject.next(data);
+    subject.complete();
+  }, 1000);
+  return subject.asObservable();
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TalentPoolService extends CrudService<TalentPoolFilter, MyTalentPool, MyTalentPool> {
+  list(filter: TalentPoolFilter): Observable<ListResult<MyTalentPool>> {
+    const data: ListResult<MyTalentPool> = {
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(id => new MyTalentPool().fake(id)),
+      length: 10
+    };
+    return createSubscription(data);
+  }
+  fetch(id: number) {
+    throw new Error('Method not implemented.');
+  }
+  create(data: MyTalentPool) {
+    if (fakerStatic.random.boolean()) {
+      return createSubscription(new MessageOut('Talent Pool created successfully', true));
+    }
+    return createSubscription(new MessageOut('Error while creating talent pool', false));
+  }
+  delete(id: number) {
+    if (fakerStatic.random.boolean()) {
+      return createSubscription(new MessageOut('Talent Pool ddeleted successfully', true));
+    }
+    return createSubscription(new MessageOut('Error while deleting talent pool', false));
+  }
+
+}
