bc0bd3a4bd0ab42273a89da04a7993a0b8e6ecae
Ridwan <olarid7852@gmail.com>
2021-04-07 11:41:33 +0100

    Update

--- src/app/components/date-range/date-range.component.ts
+++ src/app/components/date-range/date-range.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
+export class DateRange{
+  start: Date;
+  end: Date;
+}
+
 @Component({
   selector: 'ngx-date-range',
   templateUrl: './date-range.component.html',
@@ -8,26 +13,39 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class DateRangeComponent implements OnInit {
   _start = new Date();
   _end = new Date();
-  @Input() set start(val){
+  @Input() set start(val) {
     this._start = val;
-  };
-  @Input() set end(val){
+  }
+  @Input() set end(val) {
     this._start = val;
-  };
+  }
   @Output() startChange = new EventEmitter<Date>();
   @Output() endChange = new EventEmitter<Date>();
-
+  @Output() valueChange = new EventEmitter<DateRange>();
+  @Input() set value(val: DateRange){
+    this._start = val.start;
+    this._end = val.end;
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  updateStart(){
-    this.startChange.emit(this._start);
+  updateValue() {
+    this.valueChange.emit({
+      start: this._start,
+      end: this._end,
+    });
   }
 
-  updateEnd(){
+  updateStart() {
+    this.startChange.emit(this._start);
+    this.updateValue();
+  }
+
+  updateEnd() {
     this.endChange.emit(this._end);
+    this.updateValue();
   }
 
 }
