fb0ac1a3a583d79f790e6f3eec050cfda0d0383f
Philip <philip@smartace.ng>
2021-04-07 16:53:39 +0100

    Updated Disbursement Module

--- src/app/modules/disbursement/disbursement.module.ts
+++ src/app/modules/disbursement/disbursement.module.ts
@@ -1,5 +1,5 @@
 import { DataServiceProxy } from 'app/_services/service-proxies';
-import { FetchAllBudgetsServiceProxy, FetchGetBudgetServiceProxy, FetchAllBudgetItemsServiceProxy, AddUpdateBudgetServiceProxy, SingleDisbursementServiceProxy, GetExpenseProjectServiceProxy, GetAllPaymentInstitutionsServiceProxy, FetchExpensesServiceProxy } from './../../_services/service-proxies';
+import { FetchAllBudgetsServiceProxy, FetchGetBudgetServiceProxy, FetchAllBudgetItemsServiceProxy, AddUpdateBudgetServiceProxy, SingleDisbursementServiceProxy, GetExpenseProjectServiceProxy, GetAllPaymentInstitutionsServiceProxy, FetchExpensesServiceProxy, AddUpdateBudgetItemServiceProxy } from './../../_services/service-proxies';
 import { UpdateItemComponent } from './budget/update-item/update-item.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -49,6 +49,7 @@ import { OverallBudgetComponent } from './overall-budget/overall-budget.componen
     DataServiceProxy,
     AddUpdateBudgetServiceProxy,
     FetchExpensesServiceProxy,
+    AddUpdateBudgetItemServiceProxy,
   ],
 
 })
