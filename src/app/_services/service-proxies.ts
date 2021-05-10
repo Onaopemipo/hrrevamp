34cd62fe618d7f49c6307edeb16b8c4c44d3ca14
Temitope Badmus <tope@smartace.ng>
2021-05-10 11:14:22 +0100

    nswagUpdate

--- src/app/_services/service-proxies.ts
+++ src/app/_services/service-proxies.ts
@@ -9105,11 +9105,11 @@ export class BulkMasterServiceProxy {
         };
 
         return this.http.request("post", url_, options_).pipe(_observableMergeMap((response_ : any) => {
-            return this.processBulkUpload(response_);
+            return this.processBulkUploadid(response_);
         })).pipe(_observableCatch((response_: any) => {
             if (response_ instanceof HttpResponseBase) {
                 try {
-                    return this.processBulkUpload(<any>response_);
+                    return this.processBulkUploadid(<any>response_);
                 } catch (e) {
                     return <Observable<MessageOutApiResult>><any>_observableThrow(e);
                 }
@@ -9118,7 +9118,7 @@ export class BulkMasterServiceProxy {
         }));
     }
 
-    protected processBulkUpload(response: HttpResponseBase): Observable<MessageOutApiResult> {
+    protected processBulkUploadid(response: HttpResponseBase): Observable<MessageOutApiResult> {
         const status = response.status;
         const responseBlob =
             response instanceof HttpResponse ? response.body :
