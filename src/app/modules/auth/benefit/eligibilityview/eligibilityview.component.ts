2f665d6b2a485e9d6a3eccff0a454f599d615cbe
Temitope Badmus <tope@smartace.ng>
2021-05-22 12:51:11 +0100

    update

--- src/app/modules/auth/benefit/eligibilityview/eligibilityview.component.ts
+++ src/app/modules/auth/benefit/eligibilityview/eligibilityview.component.ts
@@ -59,7 +59,7 @@ export class EligibilityviewComponent implements OnInit {
       ).toPromise();
     if (!data.hasError) {
       this.eligibility = data.result;
-      this.Eli =new ManageBenefitEligibilityDTO({ ...this.eligibility.clone(), employees: ''});
+      this.Eli =new ManageBenefitEligibilityDTO({ ...this.eligibility.clone(), employees: '', benefitTypeId: 0,positionId:0});
       console.log("elo", this.eligibility);
     }
   }
